a7431f3136c3d5157e88689ff7ed794d6aa11b15
seaniiio <cnc4750@naver.com>
2024-03-26 23:34:38 +0900

    design: lot button hover옵션 추가

--- src/Component/Lot/Lot.js
+++ src/Component/Lot/Lot.js
@@ -2,23 +2,13 @@ import React from "react";
 import "./Lot.css";
 
 const Lot = ({ number, state }) => {
-  let buttonColor = "#1E4A9F";
-  if (state === "abnormal") {
-    buttonColor = "#CF3D3D";
-  }
+  let buttonColor = state === "abnormal" ? "abnormal" : "normal";
 
   return (
     <button
+      className={`lot-button ${buttonColor}`}
       style={{
-        width: "70px",
-        height: "70px",
-        backgroundColor: buttonColor,
-        borderRadius: "50%",
-        border: "none",
-        cursor: "pointer",
-        color: "white",
-        fontSize: "40px",
-        fontWeight: "bolder",
+        backgroundColor: state === "abnormal" ? "#CF3D3D" : "#1E4A9F",
       }}>
       {number}
     </button>
