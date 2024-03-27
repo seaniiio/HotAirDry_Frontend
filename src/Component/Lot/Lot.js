a2b6313641ded322d7a976e87c7f46ab78b0b21d
seaniiio <cnc4750@naver.com>
2024-03-27 16:39:48 +0900

    design: Lot Comoponent styled-component로 변경 + hover

--- src/Component/Lot/Lot.js
+++ src/Component/Lot/Lot.js
@@ -1,17 +1,25 @@
 import React from "react";
-import "./Lot.css";
+//import "./Lot.css";
+import styled from "styled-components";
+
+const LotButton = styled.button`
+  width: 70px;
+  height: 70px;
+  border: none;
+  cursor: pointer;
+  color: white;
+  font-size: 40px;
+  font-weight: bolder;
+  border-radius: 50%;
+  background-color: ${(props) =>
+    props.state === "abnormal" ? "#CF3D3D" : "#1E4A9F"};
+  &:hover {
+    background-color: ${(props) =>
+      props.state === "abnormal" ? "#B80000" : "#003366"};
+  }
+`;
 
 const Lot = ({ number, state }) => {
-  let buttonColor = state === "abnormal" ? "abnormal" : "normal";
-
-  return (
-    <button
-      className={`lot-button ${buttonColor}`}
-      style={{
-        backgroundColor: state === "abnormal" ? "#CF3D3D" : "#1E4A9F",
-      }}>
-      {number}
-    </button>
-  );
+  return <LotButton state={state}>{number}</LotButton>;
 };
 export default Lot;
